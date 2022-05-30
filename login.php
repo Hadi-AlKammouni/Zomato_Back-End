1f4b98260fd2045cc9654a18d32994140efa98c6
jad amine <jade1992amine@gmail.com>
2022-05-30 19:17:52 +0300

    Linked Sign-up to Back-end

--- login.php
+++ login.php
@@ -15,29 +15,7 @@
       $json = json_encode($user);
       echo $json;
     } else{
-      // echo "Wrong Username or Password";
-      // $response = [];
-      // $response['message'] = false;
+      die('bye');
     }
-    // // fetch the result in array bcz we only return one row
-    // $user = mysqli_fetch_assoc($test); 
-
-    // if($user){
-    //   echo 'User already exists, Choose another email address';
-    // } else {
-    //   $type = 'user';
-      
-    //   $sql = "INSERT INTO  users(name, email, password, type) VALUES('$name', '$email', '$password', '$type')";
-
-    //     //save to db and check
-    //   if(mysqli_query($conn, $sql)){
-    //     // success
-    //     header('Location: sign_up.php');
-    //   } else{
-    //     // error
-    //     echo 'query error: ' . mysqli_error($conn);
-    //   }
-    //   echo "User has been added"
-    // }
   }
 ?>
--- sign_up.php
+++ sign_up.php
@@ -1,6 +1,6 @@
 <?php
    header('Access-Control-Allow-Origin: *');
-   // header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+   header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
    // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
    //    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
    //        // may also be using PUT, PATCH, HEAD etc
@@ -15,33 +15,32 @@
 
    // connect to the db
    include './config/connect_db.php';
-
-   if(isset($_POST['name'])){
       
+   if(isset($_POST['name'])){
       $name = mysqli_real_escape_string($conn, $_POST['name']);
-
+      
       $email = mysqli_real_escape_string($conn, $_POST['email']);
+      
       $test = mysqli_query($conn, "SELECT * FROM users WHERE email= '$email'");
       
       // fetch the result in array bcz we only return one row
       $user = mysqli_fetch_assoc($test); 
-
+      
       if($user){
          echo 'User already exists, Choose another email address';
       } else {
          $type = 'user';
-         $password = hash("sha256",mysqli_real_escape_string($conn, $_POST['password']));
+         $password = hash("sha256",mysqli_real_escape_string($conn, $_POST['pass']));
          $sql = "INSERT INTO  users(name, email, password, type) VALUES('$name', '$email', '$password', '$type')";
-
+         
          //save to db and check
          if(mysqli_query($conn, $sql)){
             // success
-            header('Location: sign_up.php');
-         } else{
+            echo "new user created";
+         }else{
             // error
-            echo 'query error: ' . mysqli_error($conn);
+            echo 'query error: ';
          }
-         echo "User has been added"
       }
    }
 ?>
